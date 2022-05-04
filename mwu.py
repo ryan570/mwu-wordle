a20d29c8887467166d2bbf3f73248b9f83fc2119
Ryan <brownryan570@gmail.com>
2022-05-04 14:37:51 -0400

    first version of mwu ?

--- mwu.py
+++ mwu.py
@@ -1,4 +1,31 @@
-import dictionary
-from wordle import Wordle
+import numpy as np
+import math
+import csv
 
-print(len(dictionary.words))
+T = 5
+N = 5
+
+epsilon = math.sqrt(math.log(N) / T)
+rho = 5 # ?
+weights = [1] * N
+
+guess_counts = [ [] for i in range(N) ]
+with open('guesses.csv', newline='') as csvfile:
+	csv_reader = csv.reader(csvfile, delimiter=',')
+	next(csv_reader) # skip row with column headers
+
+	for row in csv_reader:
+		guess_counts[0].append(int(row[0]))
+		guess_counts[1].append(int(row[1]))
+		guess_counts[2].append(int(row[2]))
+		guess_counts[3].append(int(row[3]))
+		guess_counts[4].append(int(row[4]))
+
+for t in range(T):
+    probs = [w / sum(weights) for w in weights]
+    chosen = np.random.choice(N, p=probs)
+
+    outcome = guess_counts[chosen][t]
+    weights[chosen] = weights[chosen] * (1 - epsilon * ((outcome - 3.92) / rho))   
+
+print(weights)
