4f266cb978eda857ddbcfbfe910661f78802e352
Luke Duan <lduan11@live.unc.edu>
2022-05-05 00:13:25 -0400

    More documentation

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,20 @@
 from re import match as rematch
 
+# Retrieve word list
 with open('lists/answers.txt') as f:
     words = f.read().splitlines()[2:]
 
 def match(pattern, guess):
     """Matches a particular pattern with a guess to cut down on 'words' list."""
     # exact = [letter if pattern[i] == 2 else '.' for i, letter in enumerate(guess)]
+
+    # Handle correct letter in wrong position (yellow)
     contains = [letter for i, letter in enumerate(guess) if pattern[i] == 1]
+
+    # Handle letters that don't exist in the word (gray)
     block = [letter for i, letter in enumerate(guess) if pattern[i] == 0]
 
+    # Create regex pattern
     p = ""
     for c in contains:
         p += f'(?=.*{c}.*)'
@@ -22,7 +28,8 @@ def match(pattern, guess):
         else:
             p += '.'
 
-    test = list(filter(lambda m: rematch(p, m), words))
-    return set(test)
+    # Pattern matching to find remaining possible words
+    remaining = list(filter(lambda m: rematch(p, m), words))
+    return set(remaining)
 
 # print(match([0, 1, 0, 0, 0], 'salet') & match([0, 0, 1, 0, 1], 'curio') & match([2, 1, 0, 1, 0], 'arbor'))
